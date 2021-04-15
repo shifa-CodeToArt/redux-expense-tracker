1a88b091c1d1dc13a0a15fffeb9dddc95e47fa02
Shifa Shaikh <shifa@codetoart.com>
2021-04-15 10:46:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -8,19 +8,7 @@ import { setTextFilter } from "./Actions/Filters";
 
 function App() {
   const store = configureStore;
-  // store.dispatch(addExpense({ description: "Gas Bill" ,amount:1000,CreatedAt:"1618986600000"}));
-  // store.dispatch(addExpense({ description: "Water Bill",amount:8000,CreatedAt:"1618122600000"}));
-  //  store.dispatch(addExpense({ description: "Rent",amount:5000,CreatedAt:"1617258600000"}));
-  // store.dispatch(setTextFilter(""));
 
-// setTimeout(()=>{
-//   store.dispatch(setTextFilter("Bill"));
-// },3000)
-
-  // const state = store.getState();
-  // const VisibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-
-  // console.log(VisibleExpenses);
 
   return (
     <>
